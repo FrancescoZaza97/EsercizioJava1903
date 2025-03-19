d328478a7c1595c1d0d0b037b0d6e2e7ca8766f1
Checco <francesco.zaza0510@gmail.com>
2025-03-19 12:00:33 +0100

    aggiunta struttura switch x dividerci i compiti

---
+++ Esercizio2Gruppo1/src/SegniZodiacali.java
@@ -0,0 +1,39 @@
+public class SegniZodiacali {
+    public int giorno;
+    public String mese;
+
+    public void mostraSegnoZodiacale(){
+        switch(mese){
+            // Checco
+            case "gennaio":
+
+                break;
+            case "febbraio":
+                break;
+            case "marzo":
+                break;
+            //torcia umana
+            case "aprile":
+                break;
+            case "maggio":
+                break;
+            case "giugno":
+                break;
+            // Gabri
+            case "luglio":
+                break;
+            case "agosto":
+                break;
+            case "settembre":
+                break;
+            //stefano
+            case "ottobre":
+                break;
+            case "novembre":
+                break;
+            case "dicembre":
+                break;
+        }
+    }    
+
+}
---
+++ Esercizio2Gruppo1/src/SegniZodiacaliTest.java
@@ -0,0 +1,5 @@
+public class SegniZodiacaliTest {
+    public static void main(String[] args) {
+        
+    }
+}
