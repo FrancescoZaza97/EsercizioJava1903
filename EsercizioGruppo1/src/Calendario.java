54ddf6c3411f5a5e4ed383aad0c0fc4bd2d3971e
Checco <francesco.zaza0510@gmail.com>
2025-03-19 09:58:45 +0100

    aggiunti metodi di controllo per marzo e febbraio con relativa modifica di nome delle classi

--- EsercizioGruppo1/src/Calendario.java
+++ EsercizioGruppo1/src/Calendario.java
@@ -9,14 +9,24 @@ public class Calendario {
                 if(giorno >= 1 && giorno <= 12){
                     System.out.println("Corso non ancora avviato");
                 }else if(giorno >= 13 && giorno <= 28){
-                    System.out.println("il Corso ha effettuato le leszioni di Web Design");
+                    System.out.println("il Corso ha effettuato le lezioni di Web Design");
                 }else{
                     System.out.println("febbraio ha solo 28 giorni, checka bene DAEI!!!!");
                 }
                 break;
             case "marzo":
                 if (giorno >= 1 && giorno <= 4){
-                    System.out.println("il Corso ha effettuato le leszioni di Web Design");
+                    System.out.println("il Corso ha effettuato le lezioni di Web Design");
+                } else if(giorno == 5 ){
+                    System.out.println("il Corso ha effettuato le lezioni di DASA");
+                } else if(giorno >= 6 && giorno <= 10){
+                    System.out.println("il Corso ha effettuato le lezioni di Java");
+                } else if (giorno == 11){
+                    System.out.println("il Corso ha effettuato le leszioni di GitHub");
+                }else if ( giorno >= 12 && giorno <= 31){
+                    System.out.println("il Corso ha effettuato le leszioni di Java");
+                } else{
+                    System.out.println("Marzo ha solo 31 giorni, checka bene DAEI!!!!");
                 }
                 break;
             case "aprile":
