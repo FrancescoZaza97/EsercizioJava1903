472900cd3ece7478f478ddbaa52c96164f0975f9
FlaminemFlam <fiammetta.degliesposti@gmail.com>
2025-03-19 11:22:46 +0100

    Aggiunta di mesi da giugno a dicembre

--- EsercizioGruppo1/src/Calendario.java
+++ EsercizioGruppo1/src/Calendario.java
@@ -2,53 +2,52 @@ public class Calendario {
     public int giorno;
     public String mese;
 
-
-    public void stampaCalendario(){
+    public void stampaCalendario() {
         switch (mese) {
             case "gennaio":
-                if(giorno >= 1 && giorno <= 31){
+                if (giorno >= 1 && giorno <= 31) {
                     System.out.println("Corso non ancora avviato");
-                }else{
+                } else {
                     System.out.println("Data non esistente");
                 }
                 break;
-                
+
             case "febbraio":
-                if(giorno >= 1 && giorno <= 12){
+                if (giorno >= 1 && giorno <= 12) {
                     System.out.println("Corso non ancora avviato");
-                }else if(giorno >= 13 && giorno <= 28){
+                } else if (giorno >= 13 && giorno <= 28) {
                     System.out.println("il Corso ha effettuato le lezioni di Web Design");
-                }else{
+                } else {
                     System.out.println("febbraio ha solo 28 giorni, checka bene DAEI!!!!");
                 }
                 break;
 
             case "marzo":
-                if (giorno >= 1 && giorno <= 4){
+                if (giorno >= 1 && giorno <= 4) {
                     System.out.println("il Corso ha effettuato le lezioni di Web Design");
-                } else if(giorno == 5 ){
+                } else if (giorno == 5) {
                     System.out.println("il Corso ha effettuato le lezioni di DASA");
-                } else if(giorno >= 6 && giorno <= 10){
+                } else if (giorno >= 6 && giorno <= 10) {
                     System.out.println("il Corso ha effettuato le lezioni di Java");
-                } else if (giorno == 11){
+                } else if (giorno == 11) {
                     System.out.println("il Corso ha effettuato le lezioni di GitHub");
-                }else if ( giorno >= 12 && giorno <= 31){
+                } else if (giorno >= 12 && giorno <= 31) {
                     System.out.println("il Corso ha effettuato le lezioni di Java");
-                } else{
+                } else {
                     System.out.println("Marzo ha solo 31 giorni, checka bene DAEI!!!!");
                 }
                 break;
 
             case "aprile":
-                if (giorno >= 1 && giorno <= 8){
+                if (giorno >= 1 && giorno <= 8) {
                     System.out.println("Il corso ha effettuato le lezioni di Java");
-                } else if(giorno >= 9 && giorno <= 17) {
+                } else if (giorno >= 9 && giorno <= 17) {
                     System.out.println("Il corso ha effettuato le lezioni di Database");
-                } else if(giorno >= 18 && giorno <= 20 || giorno == 22 || giorno == 23) {
+                } else if (giorno >= 18 && giorno <= 20 || giorno == 22 || giorno == 23) {
                     System.out.println("Il corso ha effettuato lezioni di JDBC");
-                } else if (giorno == 21 || giorno == 25){
+                } else if (giorno == 21 || giorno == 25) {
                     System.out.println("Festivo");
-                }else if (giorno == 24 || giorno >= 26 && giorno <= 30) {
+                } else if (giorno == 24 || giorno >= 26 && giorno <= 30) {
                     System.out.println("Il corso ha effettuato lezioni di Spring");
                 } else {
                     System.out.println("Data non valida!");
@@ -61,14 +60,70 @@ public class Calendario {
                 } else if (giorno >= 2 && giorno <= 8) {
                     System.out.println("il corso ha effettuato le lezioni di spring");
                 } else if (giorno >= 9 && giorno <= 21) {
-                    System.out.println("il corso ha effettuato il project work");    
-                } else if (giorno >= 22 && giorno <=31){
+                    System.out.println("il corso ha effettuato il project work");
+                } else if (giorno >= 22 && giorno <= 31) {
                     System.out.println("il corso e' stato terminato");
                 } else {
                     System.out.println("Data non esistente");
                 }
                 break;
 
+            case "giugno":
+                if (giorno >= 1 && giorno <= 30) {
+                    System.out.println("Corso non presente");
+                } else {
+                    System.out.println("Data non valida");
+                }
+                break;
+
+            case "luglio":
+                if (giorno >= 1 && giorno <= 31) {
+                    System.out.println("Corso non presente");
+                } else {
+                    System.out.println("Data non valida");
+                }
+                break;
+
+            case "agosto":
+                if (giorno >= 1 && giorno <= 31) {
+                    System.out.println("Corso non presente");
+                } else {
+                    System.out.println("Data non valida");
+                }
+                break;
+            
+            case "settembre":
+                if (giorno >= 1 && giorno <= 30) {
+                    System.out.println("Corso non presente");
+                } else {
+                    System.out.println("Data non valida");
+                }
+                break;
+
+            case "ottobre":
+                if (giorno >= 1 && giorno <= 31) {
+                    System.out.println("Corso non presente");
+                } else {
+                    System.out.println("Data non valida");
+                }
+                break;
+
+            case "novembre":
+                if (giorno >= 1 && giorno <= 30) {
+                    System.out.println("Corso non presente");
+                } else {
+                    System.out.println("Data non valida");
+                }
+                break;
+
+            case "dicembre":
+                if (giorno >= 1 && giorno <= 31) {
+                    System.out.println("Corso non presente");
+                } else {
+                    System.out.println("Data non valida");
+                }
+                break;
+                
             default:
                 System.out.println("il mese inserito non e' valido, Riprova!");
                 break;
