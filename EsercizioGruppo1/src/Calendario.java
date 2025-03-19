7069ad2a06d184fa13cf36bc9eabc68cba6bb2e9
Checco <francesco.zaza0510@gmail.com>
2025-03-19 09:50:37 +0100

    creazione struttura della classe Calendario con annesso Switch di controllo

--- EsercizioGruppo1/src/Calendario.java
+++ EsercizioGruppo1/src/Calendario.java
@@ -1,3 +1,33 @@
 public class Calendario {
-    
+    public int giorno;
+    public String mese;
+
+
+    public void stampaCalendario(){
+        switch (mese) {
+            case "febbraio":
+                if(giorno >= 1 && giorno <= 12){
+                    System.out.println("Corso non ancora avviato");
+                }else if(giorno >= 13 && giorno <= 28){
+                    System.out.println("il Corso ha effettuato le leszioni di Web Design");
+                }else{
+                    System.out.println("febbraio ha solo 28 giorni, checka bene DAEI!!!!");
+                }
+                break;
+            case "marzo":
+                if (giorno >= 1 && giorno <= 4){
+                    System.out.println("il Corso ha effettuato le leszioni di Web Design");
+                }
+                break;
+            case "aprile":
+                
+                break;   
+            case "maggio":
+                
+                break;
+            default:
+                System.out.println("il mese inserito non e' valido, Riprova!");
+                break;
+        }
+    }
 }
