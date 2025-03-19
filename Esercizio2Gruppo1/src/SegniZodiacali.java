936047bf0ca5d561c1e451842de40516ad6590d4
stefaniello <stefanovalentini591@gmail.com>
2025-03-19 12:12:48 +0100

    Update SegniZodiacali.java

--- Esercizio2Gruppo1/src/SegniZodiacali.java
+++ Esercizio2Gruppo1/src/SegniZodiacali.java
@@ -28,10 +28,25 @@ public class SegniZodiacali {
                 break;
             //stefano
             case "ottobre":
+                if (giorno <= 22) {
+                    System.out.println("il tuo segno zodiacale è bilancia");
+                } else if (giorno >= 23) {
+                    System.out.println("Il tuo segno zodiacale è scorpione");
+                }
                 break;
             case "novembre":
+                if (giorno <= 21) {
+                    System.out.println("il tuo segno zodiacale è scorpione");
+                } else if (giorno >= 22) {
+                    System.out.println("Il tuo segno zodiacale è sagittario");
+                }
                 break;
             case "dicembre":
+                if (giorno <= 21) {
+                    System.out.println("il tuo segno zodiacale è sagittario");
+                } else if (giorno >= 22) {
+                    System.out.println("Il tuo segno zodiacale è capricorno");
+                }
                 break;
         }
     }    
