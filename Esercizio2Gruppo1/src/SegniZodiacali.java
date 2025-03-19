bae8e3b62657620391f59c7f876b83e31abb0203
Gabrisw8 <gabri.sw8@gmail.com>
2025-03-19 12:13:49 +0100

    Segni zodiacali nei giorni di: luglio, agosto e settembre

--- Esercizio2Gruppo1/src/SegniZodiacali.java
+++ Esercizio2Gruppo1/src/SegniZodiacali.java
@@ -21,10 +21,31 @@ public class SegniZodiacali {
                 break;
             // Gabri
             case "luglio":
+                if(giorno>=1 && giorno<=22){
+                    System.out.println("Il tuo sogno è: ♋ Cancro ");
+                }else if(giorno>=23 && giorno <=31) {
+                    System.out.println("Il tuo sogno è: ♌ Leone ");
+                } else {
+                    System.out.println("Giorno selezionato non esistente");
+                }
                 break;
             case "agosto":
+                if(giorno>=1 && giorno<=22){
+                    System.out.println("Il tuo sogno è: ♌ Leone ");
+                }else if(giorno>=23 && giorno <=31) {
+                    System.out.println("Il tuo sogno è: ♍ Vergine ");
+                } else {
+                    System.out.println("Giorno selezionato non esistente");
+                }
                 break;
             case "settembre":
+                if(giorno>=1 && giorno<=22){
+                    System.out.println("Il tuo sogno è: ♍ Vergine ");
+                }else if(giorno>=23 && giorno <=30) {
+                    System.out.println("Il tuo sogno è: ♎ Bilancia ");
+                } else {
+                    System.out.println("Giorno selezionato non esistente");
+                }
                 break;
             //stefano
             case "ottobre":
